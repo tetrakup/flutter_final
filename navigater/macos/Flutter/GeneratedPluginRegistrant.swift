e4f7f7847401b4774753f3bda851fc426bd42ebe
tetrakup <ataysuatceylan@gmail.com>
2024-01-31 23:35:35 +0300

    url launcher lib için entegrasyon yapildi.

--- navigater/macos/Flutter/GeneratedPluginRegistrant.swift
+++ navigater/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import device_info_plus
 import flutter_secure_storage_macos
 import path_provider_foundation
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
